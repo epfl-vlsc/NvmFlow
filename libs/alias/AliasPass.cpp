2d6a54e6503c14b399a771f6016a7752d9f03a28
daksunt <davidteksenaksun@gmail.com>
2019-08-22 15:01:28 +0200

    add independent pass to observe alias values

---
+++ libs/alias/AliasPass.cpp
@@ -0,0 +1,62 @@
+
+
+#include "AliasPass.h"
+#include "Common.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/Analysis/BasicAliasAnalysis.h"
+#include "llvm/Analysis/CFLAndersAliasAnalysis.h"
+#include "llvm/Analysis/CFLSteensAliasAnalysis.h"
+
+#include "analysis_util/AliasGroups.h"
+#include "analysis_util/DfUtil.h"
+#include "analysis_util/MemoryUtil.h"
+
+#include <cassert>
+#include <set>
+using namespace std;
+namespace llvm {
+
+void AliasPass::print(raw_ostream& OS, const Module* m) const { OS << "pass\n"; }
+
+void addToAgs(Function& F, AliasGroups& ags) {
+  for (Instruction& I : instructions(F)) {
+    if (auto* si = dyn_cast<StoreInst>(&I)) {
+      ags.add(si);
+    } else if (auto* ci = dyn_cast<CallInst>(&I)) {
+      ags.add(ci);
+      auto* f = ci->getCalledFunction();
+      addToAgs(*f, ags);
+    }
+  }
+}
+
+void runOnFunction(Function& F, AAResults& AAR) {
+  AliasGroups ags;
+  addToAgs(F, ags);
+  ags.createGroups(&AAR);
+  ags.print(errs());
+}
+
+bool AliasPass::runOnModule(Module& M) {
+  AAResults AAR(getAnalysis<TargetLibraryInfoWrapperPass>().getTLI());
+  auto& cflResult = getAnalysis<CFLAndersAAWrapperPass>().getResult();
+  AAR.addAAResult(cflResult);
+
+  for (auto& F : M) {
+    runOnFunction(F, AAR);
+  }
+
+  return false;
+}
+
+void AliasPass::getAnalysisUsage(AnalysisUsage& AU) const {
+  AU.addRequired<TargetLibraryInfoWrapperPass>();
+  AU.addRequired<CFLAndersAAWrapperPass>();
+
+  AU.setPreservesAll();
+}
+
+char AliasPass::ID = 0;
+RegisterPass<AliasPass> X("alias", "Alias pass");
+
+} // namespace llvm
---
+++ libs/alias/AliasPass.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Common.h"
+
+namespace llvm {
+
+struct AliasPass : public ModulePass {
+  static char ID;
+
+  AliasPass() : ModulePass(ID) {}
+
+  bool runOnModule(Module& M) override;
+
+  void print(raw_ostream &OS, const Module *m) const override;
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override;
+};
+
+} // namespace llvm
