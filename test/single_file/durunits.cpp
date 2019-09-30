f2821d13ad800ec46a1530830b3db8b578a18417
daksunt <davidteksenaksun@gmail.com>
2019-09-30 14:01:14 +0200

    fix unit tests

--- test/single_file/durunits.cpp
+++ test/single_file/durunits.cpp
@@ -61,3 +61,50 @@ struct Dur {
     writeIpa(data);
   }
 };
+
+struct Data {
+  int data;
+  Data* next;
+};
+
+struct Dur2 {
+  dur_field Data* next;
+
+  void nvm_fnc correct() {
+    auto* ptr = new Data();
+    pm_flushfence(ptr);
+    pfence();
+    next = ptr;
+  }
+
+  void nvm_fnc fenceNotFlushedData(Data* ptr) { next = ptr; }
+
+  void nvm_fnc correctParam(Data* ptr) {
+    pm_flushfence(ptr);
+    next = ptr;
+  }
+
+  void nvm_fnc correctObj() {
+    auto* data = new Data();
+    data->data = 5;
+
+    pm_flushfence(data);
+    next = data;
+  }
+
+  void nvm_fnc writeObj() {
+    auto* data = new Data();
+    data->data = 5;
+
+    pm_flushfence(data);
+    data->data = 5;
+
+    next = data;
+  }
+
+  void nvm_fnc branchFlush(Data* ptr) {
+    if (ptr->data == 1)
+      pm_flushfence(ptr);
+    next = ptr;
+  }
+};
--- test/single_file/test.cpp
+++ test/single_file/test.cpp
@@ -1,57 +1,13 @@
 #include "annot.h"
 
-struct Dur {
-  dur_field int* next;
-  int* next2;
+struct Dcl {
+  int data[5];
+  
 
   void nvm_fnc correct() {
-    auto* ptr = new int(5);
-    pm_clflushopt(ptr);
-    pfence();
-    next = ptr;
-  }
-
-  void nvm_fnc doubleFlush() {
-    auto* ptr = new int(5);
-    pm_clflushopt(ptr);
-    pfence();
-    pm_clflushopt(ptr);
-    next = ptr;
-  }
-
-  void nvm_fnc doubleFlushParam(int* ptr) {
-    pm_clflushopt(ptr);
-    pfence();
-    pm_clflushopt(ptr);
-    next = ptr;
-  }
-
-  void nvm_fnc correctParam(int* ptr) {
-    pm_clflush(ptr);
-    next = ptr;
-  }
-
-  void nvm_fnc correctInt() {
-    auto* data = new int(5);
-
-    pm_clflush(data);
-    next = data;
-  }
-
-  void nvm_fnc writeInt() {
-    auto* data = new int(5);
-
-    pm_clflush(data);
-    *data = 5;
-
-    next = data;
-  }
-
-  void writeIpa(int* ptr) { next = ptr; }
-
-  void nvm_fnc ipa() {
-    auto* data = new int(5);
-    pm_clflushopt(data);
-    writeIpa(data);
+    int i = (int)cond();
+    data[i] = 4;
   }
 };
+
+
