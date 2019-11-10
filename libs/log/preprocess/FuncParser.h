d804dd3e1249a42e850d5ef6bcecfdec250ed586
daksunt <davidteksenaksun@gmail.com>
2019-11-10 19:31:59 +0100

    simplify funcparser

--- libs/log/preprocess/FuncParser.h
+++ libs/log/preprocess/FuncParser.h
@@ -7,9 +7,8 @@ namespace llvm {
 template <typename Globals> class FuncParser : public FunctionParser<Globals> {
   using Base = FunctionParser<Globals>;
 
-  static constexpr const char* skipNames[] = {
-      "pmemobj_direct", "pmemobj_pool_by_oid",
-      "_ZN18tree_map_node_toidC2Ev", "_ZN18tree_map_node_toidC2E7pmemoid"};
+  static constexpr const char* skipNames[] = {"pmemobj_direct",
+                                              "pmemobj_pool_by_oid"};
 
   bool isKnownSkipFunction(Function* f) const override {
     for (auto* skipName : skipNames) {
